dc498f2cd3b516d1147d3380f4f71b69636535be
hanabeeeee <105402245+hanabeeeee@users.noreply.github.com>
2023-01-19 17:25:16 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,40 @@ window.onload = function() {
     AOS.init();
   }
   
+
+  var interleaveOffset = 0.5;
+
+  $(document).ready(function(){
+    const swiper = new Swiper('#interior .swiper', {
+      spaceBetween: 30,
+      centeredSlides: true,
+
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        
+        direction: 'horizontal',  // 효과
+        loop: true,
+
+        effect: 'fade',
+        
+      // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        
+      });
+});
+
+
+$('.swiper').on('mouseenter',function(){
+  swiper.autoplay.stop();
+});
+
+$('.swiper').on('mouseleave',function(){
+  swiper.autoplay.start();
+  
+});
+
