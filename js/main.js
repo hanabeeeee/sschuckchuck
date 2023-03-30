634409e58456eae1667fe20d2840dcf658e2f045
hanabeeeee <105402245+hanabeeeee@users.noreply.github.com>
2023-03-30 11:49:14 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -31,3 +31,29 @@ $(document).ready(function(){
      });
 });
 
+$(document).ready(function(){
+  const swiper_ = new Swiper('.swiper-', {
+    spaceBetween: 30,
+    centeredSlides: true,
+
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      
+      direction: 'horizontal',  // 효과
+      loop: true,
+
+      effect: 'fade',
+
+      navigation: {   // 버튼 사용자 지정
+
+        nextEl: '.swiper-button-next',
+       
+        prevEl: '.swiper-button-prev',
+      },
+
+      
+    });
+});
+
